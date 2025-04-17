3a401911e1741b08f3686a5c227bc084dec44a20
Cole Flannery <jaidencoleflannery@gmail.com>
2025-04-16 22:02:56 -0500

    Corrective push

--- server/obj/Debug/net8.0/server.AssemblyInfo.cs
+++ server/obj/Debug/net8.0/server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+feca52e26f73636186c2d3aeec9dbe454367a3bc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de2623185c0c87c9a574beadb9032633a58e5a70")]
 [assembly: System.Reflection.AssemblyProductAttribute("server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
